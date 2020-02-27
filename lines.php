5c4b214e76960ca3f6bb0d220bebeeed30230c34
luciano <lucsoaam@gmail.com>
2020-02-26 22:38:20 -0400

    Starting to create the notifications.

--- lines.php
+++ lines.php
@@ -3,84 +3,97 @@ include("conexao.php");
 
  $pdo_verifica = $conexao_pdo->prepare("select idlinha, linha, status from linhas ");
                      $pdo_verifica->execute();
+$linhastatus = array();
+$cor = array();
+$i = 0;
+$som = 0;
             while($fetch = $pdo_verifica->fetch()){
-            
+            $i= $i+1;
                 //echo '<td>' . $fetch['linha'] . '</td><br>';
-                $linha = array($fetch['idlinha'] => $fetch['status']);
-                
-                
+                $linhastatus[$i] = $fetch['status'];
+                //echo ("Linha ".$i . " = ". $linhastatus[$i]);
+                if($linhastatus[$i] == 1 and $som == 0){
+                    $cor[$i] = 'style="background-color: red; font-weight: bolder"';
+                    $som = 1;
+                }else{
+                    $cor[$i] = '';
+                }
+                if($som == 1){
+                    $som = 2;
+                    ?><bgsound src="piano-notification.mp3" -1> <?php
+                }
                 
             }
 
-    echo $linha["1"];
+
     
 ?>
 
 
-<div class="inner">
-                    <nav>
-                        <ul>
-                            <li style="background-color: red" style=""><a>Watch 01</a></li>
-                            <li><a>Watch 02</a></li>
-                            <li><a></a></li>
-                            <li><a></a></li>
-                            <li><a></a></li>
-                            <!--<li><a href="#elements">Elements</a></li>-->
-                        </ul>
-                    </nav>
-                    <br>
-                    <nav>
-                        <ul>
-                            <li><a>J003</a></li>
-                            <li><a>J004</a></li>
-                            <li><a>J005</a></li>
-                            <li><a>J006</a></li>
-                            <li><a>J007</a></li>
-                            <!--<li><a href="#elements">Elements</a></li>-->
-                        </ul>
-                    </nav>
-                    <br>
-                    <nav>
-                        <ul>
-                            <li><a>J008</a></li>
-                            <li><a>J009</a></li>
-                            <li><a>J010</a></li>
-                            <li><a>J011</a></li>
-                            <li><a>J012</a></li>
-                            <!--<li><a href="#elements">Elements</a></li>-->
-                        </ul>
-                    </nav>
-                    <br>
-                    <nav>
-                        <ul>
-                            <li><a>J013</a></li>
-                            <li><a>J014</a></li>
-                            <li><a>J015</a></li>
-                            <li><a>J016</a></li>
-                            <li><a>J017</a></li>
-                            <!--<li><a href="#elements">Elements</a></li>-->
-                        </ul>
-                    </nav>
-                    <br>
-                    <nav>
-                        <ul>
-                            <li><a>J018</a></li>
-                            <li><a>J019</a></li>
-                            <li><a>J020</a></li>
-                            <li><a>M06</a></li>
-                            <li><a>M05</a></li>
-                            <!--<li><a href="#elements">Elements</a></li>-->
-                        </ul>
-                    </nav>
-                    <br>
-                    <nav>
-                        <ul>
-                            <li><a>TM01</a></li>
-                            <li><a>M01</a></li>
-                            <li><a>M02</a></li>
-                            <li><a>M03</a></li>
-                            <li><a>M04</a></li>
-                            <!--<li><a href="#elements">Elements</a></li>-->
-                        </ul>
-                    </nav>
-                </div>
+    <div class="inner">
+        <nav>
+            <ul>
+                <li <?php echo $cor[1]; ?>><a>Watch 01</a></li>
+                <li <?php echo $cor[2]; ?>><a>Watch 02</a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+                <!--<li><a href="#elements">Elements</a></li>-->
+            </ul>
+        </nav>
+        <br>
+        <nav>
+            <ul>
+                <li <?php echo $cor[3]; ?>><a>J003</a></li>
+                <li <?php echo $cor[4]; ?>><a>J004</a></li>
+                <li <?php echo $cor[1]; ?>><a>J005</a></li>
+                <li <?php echo $cor[1]; ?>><a>J006</a></li>
+                <li <?php echo $cor[1]; ?>><a>J007</a></li>
+                <!--<li><a href="#elements">Elements</a></li>-->
+            </ul>
+        </nav>
+        <br>
+        <nav>
+            <ul>
+                <li <?php echo $cor[1]; ?>><a>J008</a></li>
+                <li <?php echo $cor[1]; ?>><a>J009</a></li>
+                <li <?php echo $cor[1]; ?>><a>J010</a></li>
+                <li <?php echo $cor[1]; ?>><a>J011</a></li>
+                <li <?php echo $cor[1]; ?>><a>J012</a></li>
+                <!--<li><a href="#elements">Elements</a></li>-->
+            </ul>
+        </nav>
+        <br>
+        <nav>
+            <ul>
+                <li <?php echo $cor[1]; ?>><a>J013</a></li>
+                <li <?php echo $cor[1]; ?>><a>J014</a></li>
+                <li <?php echo $cor[1]; ?>><a>J015</a></li>
+                <li <?php echo $cor[1]; ?>><a>J016</a></li>
+                <li <?php echo $cor[1]; ?>><a>J017</a></li>
+                <!--<li><a href="#elements">Elements</a></li>-->
+            </ul>
+        </nav>
+        <br>
+        <nav>
+            <ul>
+                <li <?php echo $cor[1]; ?>><a>J018</a></li>
+                <li <?php echo $cor[1]; ?>><a>J019</a></li>
+                <li <?php echo $cor[1]; ?>><a>J020</a></li>
+                <li <?php echo $cor[1]; ?>><a>M06</a></li>
+                <li <?php echo $cor[1]; ?>><a>M05</a></li>
+                <!--<li><a href="#elements">Elements</a></li>-->
+            </ul>
+        </nav>
+        <br>
+        <nav>
+            <ul>
+                <li <?php echo $cor[1]; ?>><a>TM01</a></li>
+                <li <?php echo $cor[1]; ?>><a>M01</a></li>
+                <li <?php echo $cor[1]; ?>><a>M02</a></li>
+                <li <?php echo $cor[1]; ?>><a>M03</a></li>
+                <li <?php echo $cor[1]; ?>><a>M04</a></li>
+                <!--<li><a href="#elements">Elements</a></li>-->
+            </ul>
+        </nav>
+    </div>
